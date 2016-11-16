380d94887372cb0af8d5e2229151c92b0799eb83
root <rjsoni@cs.stonybrook.edu>
2016-11-16 15:14:57 -0800

    modularised code and intercepted execve

--- kdriver.c
+++ kdriver.c
@@ -20,6 +20,8 @@ unsigned long *syscall_table = NULL;
 asmlinkage int (*original_write) (unsigned int, const char __user *, size_t);
 asmlinkage long (*original_read) (unsigned int, char __user *, size_t);
 asmlinkage long (*original_open) (const char __user *, int, umode_t);
+asmlinkage long (*original_execve) (const char __user *filename, const char __user *const __user *argv, const char __user *const __user *envp);
+
 
 /* This method retreives the system-call table address in memory */
 static int get_system_call_table(char *kern_ver)
@@ -191,22 +193,23 @@ asmlinkage int new_write(unsigned int x, const char __user * y, size_t size)
 	return original_write(x, y, size);
 }
 
-asmlinkage long new_open(const char __user * path, int flags, umode_t mode)
-{
+
+bool is_file_malicious(const char *path){
 	char *kpath;
 	struct file *filp;
 	struct file_data *fdata;
 	long err = 0;
 	bool ret_val = false;
-	//printk(KERN_INFO "open hooked");
+	bool is_malicious = false;
 	kpath = get_path_name(path);
 	if (kpath == NULL) {
 		printk(KERN_ERR "KDRIVER: could not get path from user\n");
 		err = -ENOMEM;
+		is_malicious = true;
 		goto out;
 	}
 	
-	    /* lower two checks are for test purpose only */ 
+	/* lower two checks are for test purpose only */ 
 	
 	if (strncmp(kpath, "/home", 5) != 0)
 		goto out;
@@ -214,6 +217,7 @@ asmlinkage long new_open(const char __user * path, int flags, umode_t mode)
 	filp = filp_open(kpath, O_RDONLY, 0);
 	if (filp == NULL || IS_ERR(filp)) {
 		printk(KERN_ERR "Cannot open file\n");
+		is_malicious = true;
 		goto out;
 	}
 
@@ -222,6 +226,7 @@ asmlinkage long new_open(const char __user * path, int flags, umode_t mode)
 	if (fdata == NULL) {
 		printk(KERN_ERR
 			"error occured while reading from file to scan\n");
+		is_malicious = true;
 		goto out_vdef;
 	}
 	printk("FDATA: file exhausted %d\n", fdata->file_exhausted);
@@ -238,8 +243,9 @@ asmlinkage long new_open(const char __user * path, int flags, umode_t mode)
 	/* moving vdef here since we should not read virus definitions if file is white listed*/
         if (vdef == NULL) {
                vdef = read_virus_def();
-               if (vdef == NULL)
-               goto out_close;
+               if (vdef == NULL){
+	               goto out_close;
+		}
         }
 
 	err = scan(filp, fdata, vdef);
@@ -257,8 +263,26 @@ asmlinkage long new_open(const char __user * path, int flags, umode_t mode)
  out:	
 	kfree(kpath);
 	kpath = NULL;
-	return original_open(path, flags, mode);
+	return is_malicious;
 }
+
+asmlinkage long new_open(const char __user * path, int flags, umode_t mode)
+{
+	bool is_malicious = is_file_malicious(path);
+	//printk(KERN_INFO "open hooked");
+	if(!is_malicious)
+		return original_open(path, flags, mode);
+	else
+		return -EACCES; 
+}
+
+asmlinkage long new_execve(const char __user *filename, const char __user *const __user *argv, const char __user *const __user *envp)
+{
+	printk("\nIntercepted execve");
+	return original_execve(filename, argv, envp);
+}
+
+
 
 asmlinkage long new_read(unsigned int fd, char __user * path_name,
 			    size_t size)
@@ -352,9 +376,11 @@ static int __init on_init(void)
 		original_write = (void *)syscall_table[__NR_write];
 		original_read = (void *)syscall_table[__NR_read];
 		original_open = (void *)syscall_table[__NR_open];
+		original_execve = (void *)syscall_table[__NR_execve];
 	         //syscall_table[__NR_write] = (unsigned long)&new_write;
                  //syscall_table[__NR_read] = (unsigned long)&new_read;
 		syscall_table[__NR_open] = (unsigned long) &new_open;
+		syscall_table[__NR_execve] = (unsigned long) &new_execve;
 		write_cr0(read_cr0() | 0x10000);
 		printk(KERN_EMERG "[+] onload: sys_call_table hooked\n");
 		vdef = read_virus_def();
@@ -391,7 +417,7 @@ static void __exit on_exit(void)
 		    //syscall_table[__NR_write] = (unsigned long)original_write;
 		    //syscall_table[__NR_read] = (unsigned long)original_read;
 		    syscall_table[__NR_open] = (unsigned long)original_open;
-		
+		    syscall_table[__NR_execve] = (unsigned long)original_execve;
 		    /* mark the area again as read only */
 		    write_cr0(read_cr0() | 0x10000);
 		printk(KERN_EMERG "[+] on_exit: sys_call_table unhooked\n");
