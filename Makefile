5ca5857a88fc6fc0feb041009295ce58adb727e5
Sreeja Acharya <talk2sreejamohan@gmail.com>
2016-11-22 01:07:12 -0800

    Updating the antivirus-update script

--- Makefile
+++ Makefile
@@ -5,3 +5,4 @@ all:
 	make   -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+	rm popup
--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 touch /root/dummy
 pkill popup
 gcc -o popup popup.c
-./popup &
-
+./popup &	
+virus_file="/root/virus.db"
+whitelist_file="/root/whitelist.db"
+if [ ! -f "$virus_file" -o ! -f "$whitelist_file" ]
+then
+        cp 'antivirus-update' '/usr/local/bin/'
+        cp 'antivirus.properties' '/tmp/'
+        antivirus-update
+fi
 rmmod antivirus.ko
 insmod antivirus.ko
