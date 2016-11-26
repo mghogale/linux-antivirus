d8d1d2800e80dc8901e5f803c919fd5d4f1b199f
root <smohan24>
2016-11-25 17:49:37 -0800

    Removing files while unloading modules

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,8 @@
 pkill popup
 rmmod antivirus.k0
+rm -f /root/dummy
+rm -f /root/virus.db
+rm -f /root/whitelist.db
+rm -f /tmp/antivirus.properties
+rm -f /usr/local/bin/antivirus-update
 cp /dev/null /root/dummy
