d94f98b6af397380150108b01d538fbe2eb8e13a
root <you@example.com>
2016-11-27 12:32:50 -0800

    copying user to /usr/local/bin folder

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 rmmod antivirus.ko
 touch /root/dummy
 
+gcc user.c -o user
+
 virus_file="/root/virus.db"
 whitelist_file="/root/whitelist.db"
 if [ ! -f "$virus_file" -o ! -f "$whitelist_file" ]
@@ -9,13 +11,12 @@ then
         cp 'antivirus.properties' '/tmp/'
         antivirus-update
 	cp 'antivirus-scan' '/usr/local/bin/'
+	cp 'user' '/usr/local/bin/'
 fi
 
 pkill popup
 gcc -o popup popup.c
 ./popup &
-
-gcc user.c -o user
 
 rm -f /home/.CheckMark.png
 cp CheckMark.png /home/.CheckMark.png
